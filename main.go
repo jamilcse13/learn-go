b356ed689a4a154cc051eac76039f09857374c79
jamil <jamil.bd0561@gmail.com>
2023-03-02 11:35:45 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Bismillah")
+
+	fmt.Println("Go" + "Lang")
+	fmt.Println("10+7 = ", 10+7)
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+
+	// comment
+	fmt.Println(true)	// this is also a comment
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	/*
+	this is multi line comment
+	*/
+}
+
